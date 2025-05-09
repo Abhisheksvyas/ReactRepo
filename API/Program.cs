a9c7499c34f148223b32a78ad28e2cbf0efea72f
abhishek Vyas <abhisheksvyasazure@gmail.com>
2025-05-10 00:53:54 +0530

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000","https://localhost:3000"));
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
+}
+
+app.Run();
---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+function App() {
+
+ const[activites, setactivites] = useState<Activity[]>([]);
+ useEffect(() => {
+   axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then((response) => {
+        setactivites(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching activities:", error);
+      });
+     
+ }, []);
+ console.log(activites);  
+
+  return (
+    
+<>
+      <Typography variant='h3'>Reactivities</Typography>
+      <List>
+        {activites.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+
+   
+   
+  )
+}
+
+export default App
